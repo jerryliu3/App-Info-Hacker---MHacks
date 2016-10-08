af5ea263b7d7c50945926eb96b6b86e687c2d693
Jerry Liu <3jerryliu@gmail.com>
2016-10-08 12:29:52 -0400

    Add files via upload

---
+++ PowerShellCommand.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+public class PowerShellCommand {
+
+ public static void main(String[] args) throws IOException {
+
+  //String command = "powershell.exe  your command";
+  //Getting the version
+  String command = "powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File C:\\Users\\Jerry\\Desktop\\script.ps1";
+  // Executing the command
+  Process powerShellProcess = Runtime.getRuntime().exec(command);
+  // Getting the results
+  powerShellProcess.getOutputStream().close();
+  String line;
+  System.out.println("Standard Output:");
+  BufferedReader stdout = new BufferedReader(new InputStreamReader(
+    powerShellProcess.getInputStream()));
+  while ((line = stdout.readLine()) != null) {
+   System.out.println(line);
+  }
+  stdout.close();
+  System.out.println("Standard Error:");
+  BufferedReader stderr = new BufferedReader(new InputStreamReader(
+    powerShellProcess.getErrorStream()));
+  while ((line = stderr.readLine()) != null) {
+   System.out.println(line);
+  }
+  stderr.close();
+  System.out.println("Done");
+
+  }
+
+}
---
+++ script.ps1
@@ -0,0 +1,3 @@
+﻿cd C:\Users\Jerry\Downloads\android-backup-extractor-20160710-bin
+adb.exe backup -f myfile.ab com.facebook.katana
+java -jar abe.jar unpack myfile.ab myfile.tar password123
